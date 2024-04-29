51cb769d3237d98581d9838ef65ef241bba53dc8
olawaleraji <savagedotjsx@gmail.com>
2024-04-29 06:21:45 -0700

    back at it

--- app/components/Built.tsx
+++ app/components/Built.tsx
@@ -143,13 +143,6 @@ function Built ({}: Props) {
             <div className='absolute top-0 inset-x-0 z-10'>
               <div className='p-4 flex flex-col h-full sm:p-6'>
                 <div className='flex items-center'>
-                  <div className='flex-shrink-0'>
-                    <img
-                      className='size-[46px] border-2 border-white rounded-full'
-                      src='https://images.unsplash.com/photo-1669837401587-f9a4cfe3126e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80'
-                      alt='Image Description'
-                    />
-                  </div>
                   <div className='ms-2.5 sm:ms-4'>
                     <h4 className='font-semibold text-white'>Ajao Dev</h4>
                     <p className='text-xs text-white/80'>March 30, 2024</p>
